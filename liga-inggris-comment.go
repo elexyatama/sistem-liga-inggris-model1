a8872b3856fe51d151e82781e608f7b62d32f5e3
Elexya <elexyatama@gmail.com>
2023-12-29 19:58:16 +0700

    added comment version

--- liga-inggris-comment.go
+++ liga-inggris-comment.go
@@ -4,31 +4,35 @@ import "fmt"
 
 const dataLimit int = 20
 
+// club adalah struktur data untuk merepresentasikan informasi klub dalam liga sepak bola.
 type club struct {
-	name  string
-	win   int
-	lose  int
-	draw  int
-	point int //3 menang, 1 seri, 0 kalah
-	gf    int //goal for, gol yang dicetak
-	ga    int //goal against, gol yang yang dicetak tim musuh
+	name  string // Nama klub
+	win   int    // Jumlah win
+	lose  int    // Jumlah lose
+	draw  int    // Jumlah draw
+	point int    // Jumlah poin (3 untuk kemenangan, 1 untuk seri, 0 untuk kekalahan)
+	gf    int    // Jumlah gol yang dicetak (goal for)
+	ga    int    // Jumlah gol yang diterima (goal against)
 }
 
+// match adalah struktur data untuk merepresentasikan informasi pertandingan antara dua klub.
 type match struct {
-	home      club
-	away      club
-	homeScore int
-	awayScore int
+	home      club // Klub tuan rumah
+	away      club // Klub tamu
+	homeScore int  // Skor klub tuan rumah
+	awayScore int  // Skor klub tamu
 }
 
+// matchWeek adalah struktur data untuk merepresentasikan satu pekan pertandingan dalam liga.
 type matchWeek struct {
 	matches [dataLimit / 2]match
 }
 
+// league adalah struktur data untuk merepresentasikan seluruh liga sepak bola.
 type league struct {
-	schedule [dataLimit*2 - 2]matchWeek
-	clubs    [dataLimit]club
-	nClub    int
+	schedule [dataLimit*2 - 2]matchWeek // Jadwal pertandingan untuk seluruh musim
+	clubs    [dataLimit]club            // Informasi klub dalam liga
+	nClub    int                        // Jumlah klub dalam liga
 }
 
 func main() {
@@ -60,6 +64,9 @@ func main() {
 	}
 }
 
+// func seasonMenu digunakan untuk menampilkan menu utama selama musim berlangsung.
+// Menu ini memungkinkan pengguna untuk mengisi skor pertandingan, melihat papan klasemen, mengedit skor pekan sebelumnya,
+// melanjutkan ke pekan berikutnya, atau mengakhiri musim.
 func seasonMenu(leagues *league) {
 	var input int = 99
 	var gameWeek = 0
@@ -96,6 +103,8 @@ func seasonMenu(leagues *league) {
 	}
 }
 
+// func processInputClub digunakan untuk memproses input dari pengguna
+// dan menambahkan klub baru ke dalam struktur data liga jika belum mencapai batas maksimum.
 func processInputClub(leagues *league) {
 	if leagues.nClub == dataLimit {
 		fmt.Println("Tidak dapat menambahkan lebih banyak klub. Batas liga telah tercapai.")
@@ -106,12 +115,14 @@ func processInputClub(leagues *league) {
 	insertClub(leagues, newClub)
 }
 
+// func insertClub digunakan untuk menyisipkan klub baru ke dalam struktur data liga.
 func insertClub(leagues *league, newClub club) {
 	leagues.clubs[leagues.nClub] = newClub
 	leagues.nClub++
 	fmt.Println("Klub berhasil ditambahkan ke dalam liga.")
 }
 
+// func processDeleteClub digunakan untuk memproses penghapusan klub dari struktur data liga.
 func processDeleteClub(leagues *league) {
 	var clubName string
 	fmt.Print("Masukkan nama klub yang ingin dihapus: ")
@@ -125,6 +136,7 @@ func processDeleteClub(leagues *league) {
 	}
 }
 
+// func deleteClub digunakan untuk menghapus klub dari struktur data liga.
 func deleteClub(leagues *league, x int) {
 	for i := x; i < leagues.nClub-1; i++ {
 		leagues.clubs[i] = leagues.clubs[i+1]
@@ -132,6 +144,7 @@ func deleteClub(leagues *league, x int) {
 	leagues.nClub--
 }
 
+// func processEditClub digunakan untuk memproses pengeditan nama klub dalam struktur data liga.
 func processEditClub(leagues *league) {
 	var clubName string
 	fmt.Print("Masukkan nama klub yang ingin diedit: ")
@@ -150,10 +163,12 @@ func processEditClub(leagues *league) {
 	}
 }
 
+// func editClubName digunakan untuk mengubah nama klub dalam struktur data liga.
 func editClubName(leagues *league, index int, newName string) {
 	leagues.clubs[index].name = newName
 }
 
+// func searchClubName digunakan untuk mencari indeks klub berdasarkan nama menggunakan algoritma binary search.
 func searchClubName(leagues league, clubName string) int { //binary search ada disini
 	low, high := 0, leagues.nClub-1
 
@@ -172,7 +187,8 @@ func searchClubName(leagues league, clubName string) int { //binary search ada d
 	return -1
 }
 
-func sortByName(league *league) { //insertion sort disini
+// func sortByName digunakan untuk mengurutkan klub dalam struktur data liga berdasarkan nama menggunakan algoritma insertion sort.
+func sortByName(league *league) {
 	n := league.nClub
 
 	for i := 1; i < n; i++ {
@@ -186,7 +202,8 @@ func sortByName(league *league) { //insertion sort disini
 	}
 }
 
-func sortByPoints(league *league) { //selection sort disini
+// func sortByPoints digunakan untuk mengurutkan klub dalam struktur data liga berdasarkan poin menggunakan algoritma selection sort.
+func sortByPoints(league *league) {
 	n := league.nClub
 	for i := 0; i < n-1; i++ {
 		maxIndex := i
@@ -199,13 +216,16 @@ func sortByPoints(league *league) { //selection sort disini
 	}
 }
 
+// func generateSchedule digunakan untuk menghasilkan jadwal pertandingan untuk liga.
+// Jadwal pertandingan dibuat untuk setengah musim pertama dan setengah musim kedua.
+// Fungsi ini memeriksa apakah jumlah klub dalam liga genap, jika tidak, mencetak pesan kesalahan.
 func generateSchedule(leagues *league) {
 	if leagues.nClub%2 != 0 {
 		fmt.Println("Club count must be even!")
 		return
 	}
 
-	//generate week first half season
+	// Menghasilkan jadwal pertandingan untuk setengah musim pertama
 	for i := 0; i < leagues.nClub-1; i++ {
 		for j := 0; j < leagues.nClub/2; j++ {
 			leagues.schedule[i].matches[j].home = leagues.clubs[j]
@@ -216,7 +236,7 @@ func generateSchedule(leagues *league) {
 
 	rotateRobin(leagues)
 
-	//generate week second hald season
+	// Menghasilkan jadwal pertandingan untuk setengah musim kedua
 	for i := 0; i < leagues.nClub-1; i++ {
 		for j := 0; j < leagues.nClub/2; j++ {
 			leagues.schedule[i+leagues.nClub-1].matches[j].home = leagues.clubs[(leagues.nClub-1)-j]
@@ -228,6 +248,8 @@ func generateSchedule(leagues *league) {
 	rotateRobin(leagues)
 }
 
+// func rotateRobin digunakan untuk melakukan rotasi klub dalam array klub.
+// Rotasi ini diperlukan untuk menghasilkan jadwal pertandingan sesuai dengan format round-robin.
 func rotateRobin(leagues *league) {
 	var temp club = leagues.clubs[1]
 	for j := 1; j < leagues.nClub-1; j++ {
@@ -236,6 +258,8 @@ func rotateRobin(leagues *league) {
 	leagues.clubs[leagues.nClub-1] = temp
 }
 
+// func printAllGameWeek digunakan untuk mencetak semua jadwal pertandingan dalam liga.
+// Fungsi ini mencetak setiap pertandingan dalam setiap pekan dengan format yang terstruktur.
 func printAllGameWeek(leagues league) {
 	fmt.Println("---------------------------------------")
 	for i := 0; i < (leagues.nClub-1)*2; i++ {
@@ -247,18 +271,21 @@ func printAllGameWeek(leagues league) {
 	fmt.Println("---------------------------------------")
 }
 
+// func printGameWeek digunakan untuk mencetak jadwal pertandingan dalam satu pekan tertentu dalam liga.
 func printGameWeek(leagues league, gw int) {
 	for i := 0; i < leagues.nClub/2; i++ {
 		printMatch(leagues, gw, i)
 	}
 }
 
+// func printMatch digunakan untuk mencetak detail satu pertandingan dalam liga.
 func printMatch(leagues league, gw int, x int) {
 	fmt.Printf("Match %d : \n", x+1)
 	fmt.Printf("%-3s - %-3s\n", leagues.schedule[gw].matches[x].home.name, leagues.schedule[gw].matches[x].away.name)
 	fmt.Printf("%-3d - %-3d\n", leagues.schedule[gw].matches[x].homeScore, leagues.schedule[gw].matches[x].awayScore)
 }
 
+// func printLeaderboard digunakan untuk mencetak papan klasemen dalam liga.
 func printLeaderboard(l league) {
 	fmt.Printf("%-20s%-5s%-5s%-5s%-5s%-5s%-5s%-5s\n", "Club", "Win", "Draw", "Lose", "GF", "GA", "GD", "Pts")
 	for i := 0; i < l.nClub; i++ {
@@ -267,6 +294,9 @@ func printLeaderboard(l league) {
 	}
 }
 
+// func processViewLeaderboard digunakan untuk menampilkan dan mengelola papan klasemen dalam liga.
+// Papan klasemen diurutkan berdasarkan poin secara default.
+// Pengguna dapat memilih untuk mengurutkan berdasarkan nama atau memilih kembali ke menu pekan pertandingan.
 func processViewLeaderboard(leagues league) {
 	var input int = 99
 	sortByPoints(&leagues)
@@ -289,6 +319,7 @@ func processViewLeaderboard(leagues league) {
 	}
 }
 
+// func printClubNames digunakan untuk mencetak daftar nama klub dalam liga.
 func printClubNames(l league) {
 	fmt.Println("Club List:")
 	for i := 0; i < l.nClub; i++ {
@@ -296,6 +327,8 @@ func printClubNames(l league) {
 	}
 }
 
+// func processScoring digunakan untuk memproses input skor pertandingan untuk satu pekan tertentu dalam liga.
+// Skor tersebut kemudian digunakan untuk mengupdate statistik klub dan papan klasemen.
 func processScoring(leagues *league, gw int) {
 	var goalHome, goalAway int
 	for i := 0; i < leagues.nClub/2; i++ {
@@ -337,6 +370,7 @@ func processScoring(leagues *league, gw int) {
 	}
 }
 
+// func processEditScoring digunakan untuk memproses pengeditan skor pertandingan pada suatu pekan tertentu dalam liga.
 func processEditScoring(league *league, cgw int) {
 	var input int
 	for i := 0; i < cgw; i++ {
@@ -354,6 +388,7 @@ func processEditScoring(league *league, cgw int) {
 	processScoring(league, input-1)
 }
 
+// func minusScoring digunakan untuk mengurangkan statistik klub dan papan klasemen setelah pengeditan skor pertandingan.
 func minusScoring(leagues *league, gw int) {
 	var goalHome, goalAway int
 	for i := 0; i < leagues.nClub/2; i++ {
@@ -387,6 +422,7 @@ func minusScoring(leagues *league, gw int) {
 	}
 }
 
+// func processEndSeason digunakan untuk menampilkan papan klasemen akhir dan klub juara setelah musim berakhir.
 func processEndSeason(leagues league) {
 	sortByPoints(&leagues)
 	printLeaderboard(leagues)
